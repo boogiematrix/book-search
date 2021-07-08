9d4173352680b9b1c3fd684b2290c162c2c2284a
Night Chough <chicken.snuggles@gmail.com>
2021-07-08 17:21:36 -0500

    added mutations to front end

--- client/src/crud/mutations.js
+++ client/src/crud/mutations.js
@@ -1,9 +1,46 @@
 // TODO mutations.js:
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login {
+    user {
+      _id
+      name
+      bookCount
+      savedBooks
+    }
+  }
+`;
 // ADD_USER will execute the addUser mutation.
-
+export const ADD_USER = gql`
+  mutation addUser {
+    user {
+      _id
+      name
+      email
+      password
+      bookCount
+      savedBooks
+    }
+  }
+`;
 // SAVE_BOOK will execute the saveBook mutation.
-
+export const SAVE_BOOK = gql`
+  mutation saveBook {
+    user {
+      bookCount
+      savedBooks
+    }
+  }
+`;
 // REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+  mutation removeBook {
+    user {
+      bookCount
+      savedBooks
+    }
+  }
+`;
