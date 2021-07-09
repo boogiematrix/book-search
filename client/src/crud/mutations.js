6717bcfcae9c734536900f033a25488375cc0fd3
Night Chough <chicken.snuggles@gmail.com>
2021-07-08 20:57:30 -0500

    adjusted front end mutations

--- client/src/crud/mutations.js
+++ client/src/crud/mutations.js
@@ -4,43 +4,52 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login {
-    user {
-      _id
-      name
-      bookCount
-      savedBooks
+  mutation login ($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user {
+        _id
+        username
+        }
     }
   }
 `;
 // ADD_USER will execute the addUser mutation.
 export const ADD_USER = gql`
-  mutation addUser {
-    user {
-      _id
-      name
-      email
-      password
-      bookCount
-      savedBooks
+  mutation addUser ($email: String!, $username: String!, $password: String!){
+    addUser(email: $email, username: $username, password: $password) {
+        token
+        user {
+        _id
+        username
+        }
     }
   }
 `;
 // SAVE_BOOK will execute the saveBook mutation.
 export const SAVE_BOOK = gql`
-  mutation saveBook {
-    user {
-      bookCount
-      savedBooks
+  mutation saveBook($BookInput: inp) {
+    saveBook(BookInput: $BookInput) {
+        token
+        user {
+            _id
+            username
+            bookCount
+            savedBooks
+        }
     }
   }
 `;
 // REMOVE_BOOK will execute the removeBook mutation.
 export const REMOVE_BOOK = gql`
-  mutation deleteBook {
-    user {
-      bookCount
+  mutation deleteBook($bookId: String!) {
+    deleteBook(bookId: $bookId) {
+     token
+     user{
+      _id
+      username
       savedBooks
+     }
     }
   }
 `;
