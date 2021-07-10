11703d0e9068e46e3268a5936bff8268b1a96f51
Night Chough <chicken.snuggles@gmail.com>
2021-07-10 13:22:48 -0500

    updated signupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,8 +3,8 @@
 
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../crud/mutations';
+//import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -31,7 +31,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await ADD_USER(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
